93945fffd00eef93e217b621ae4782457bd88d4f
bakgaboon <bakgaboon@gmail.com>
2017-10-13 16:44:26 +0900

    p240

--- src/main/java/com/han/controller/SampleController.java
+++ src/main/java/com/han/controller/SampleController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -72,9 +74,36 @@ public class SampleController {
     }
 
     @GetMapping("/sample5")
-    public void sample5:(Model model) {
+    public void sample5(Model model) {
         String result = "SUCCESS";
 
         model.addAttribute("result", result);
+    }
+
+    @GetMapping("/sample6")
+    public void sample6(Model model) {
+        List<MemberVO> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(new MemberVO(i, "u0" + i, "p0" + i, "Hong" + i,
+                    new Timestamp(System.currentTimeMillis())));
+        }
+
+        model.addAttribute("list", list);
+
+        String result = "SUCCESS";
+        model.addAttribute("result", result);
+    }
+
+    @GetMapping("/sample7")
+    public void sample7(Model model) {
+        model.addAttribute("now", new Date());
+        model.addAttribute("price", 123456789);
+        model.addAttribute("title", "This is a just sample");
+        model.addAttribute("options", Arrays.asList("AAA", "BBB", "CCC", "DDD"));
+    }
+
+    @GetMapping("/sample8")
+    public void sample8(Model model) {
     }
 }
