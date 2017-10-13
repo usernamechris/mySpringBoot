45ef0965e9357163a86d5929d7ceeeed46b96b08
bakgaboon <bakgaboon@gmail.com>
2017-10-13 14:27:52 +0900

    p228

---
+++ src/main/java/com/han/domain/MemberVO.java
@@ -0,0 +1,16 @@
+package com.han.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+public class MemberVO {
+    private int mno;
+    private String mid;
+    private String mpw;
+    private String mname;
+    private Timestamp regdate;
+}
